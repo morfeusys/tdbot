3baabc04bc8613fb61d22315571a1c2d4fdb61e3
morfeusys <che@just-ai.com>
2022-12-19 20:11:20 +0300

    SearchChannelPostsOnMessage

---
+++ scenarios/src/main/kotlin/com/tdbot/scenario/SearchChannelPostsOnMessage.kt
@@ -0,0 +1,57 @@
+package com.tdbot.scenario
+
+import com.justai.jaicf.channel.td.TdActionContext
+import com.justai.jaicf.channel.td.client.TdTelegramApi
+import com.justai.jaicf.channel.td.hook.TdReadyHook
+import com.justai.jaicf.channel.td.isNotChat
+import com.justai.jaicf.channel.td.scenario.TdScenario
+import com.justai.jaicf.channel.td.scenario.onNewTextMessage
+import it.tdlight.jni.TdApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.Language
+import org.slf4j.LoggerFactory
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+fun SearchChannelPostsOnMessage(
+    chat: String,
+    @Language("regexp") pattern: String,
+    limit: Int = 100,
+    resultHandler: TdActionContext.(messages: List<TdApi.Message>) -> Unit
+) = TdScenario {
+    val logger = LoggerFactory.getLogger("SearchChannelPostsOnMessage_$chat")
+    var chatId: Long? = null
+
+    handle<TdReadyHook> {
+        api.send(TdApi.SearchPublicChat("@cats_cats")) { res ->
+            if (res.isError || res.get() == null) {
+                logger.error("Cannot find @cats_cats")
+            } else {
+                chatId = res.get().id
+            }
+        }
+    }
+
+    suspend fun TdTelegramApi.searchPosts(query: String) =
+        suspendCoroutine<List<TdApi.Message>> { continuation ->
+            send(TdApi.SearchChatMessages(chatId!!, query, null, 0, 0, limit, null, 0L)) { res ->
+                continuation.resume(res.get().messages.toList())
+            }
+        }
+
+    onNewTextMessage(pattern, isNotChat { chatId }) {
+        GlobalScope.launch {
+            mutableListOf<TdApi.Message>().apply {
+                when {
+                    activator.matcher.groupCount() == 0 -> listOf(request.input)
+                    else -> activator.matcher.groupCount().downTo(0).map { activator.group(it) }
+                }.filterNotNull().forEach { query ->
+                    addAll(reactions.api.searchPosts(query))
+                }
+            }.also { result ->
+                resultHandler(this@onNewTextMessage, result)
+            }
+        }
+    }
+}
