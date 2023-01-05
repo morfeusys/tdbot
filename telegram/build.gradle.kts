13359e91f8d9155cb99a79997f0f9ef09dd7b97e
morfeusys <che@just-ai.com>
2023-01-05 16:18:06 +0300

    telegram package refactoring

--- telegram/build.gradle.kts
+++ telegram/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    api("com.squareup.retrofit2:retrofit:2.3.0")
-    api("com.squareup.retrofit2:converter-gson:2.3.0")
-
-    implementation("com.squareup.okhttp3:logging-interceptor:3.8.0")
     implementation("com.just-ai.jaicf:core:1.2.2")
+
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.8.0")
 }
