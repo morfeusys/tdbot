d3f3a2a6bdacd171fa12ec43b69de78f618eb500
morfeusys <che@just-ai.com>
2023-01-06 13:39:57 +0300

    SendReasonOnCallDecline improvements

--- scenarios/src/main/kotlin/com/tdbot/scenario/SendReasonOnCallDecline.kt
+++ scenarios/src/main/kotlin/com/tdbot/scenario/SendReasonOnCallDecline.kt
@@ -1,11 +1,13 @@
 package com.tdbot.scenario
 
+import com.github.kotlintelegrambot.entities.ParseMode
 import com.justai.jaicf.channel.td.client.TdTelegramApi
 import com.justai.jaicf.channel.td.client.sendTextMessage
 import com.justai.jaicf.channel.td.scenario.createTdModel
 import com.justai.jaicf.channel.td.scenario.onReady
 import com.justai.jaicf.channel.td.scenario.onUpdateCall
-import com.justai.jaicf.model.activation.onlyIfInSession
+import com.justai.jaicf.reactions.buttons
+import com.justai.jaicf.reactions.toState
 import com.tdbot.api.TdBotScenario
 import com.tdbot.api.TdInteractiveScenario
 import com.tdbot.api.event
@@ -15,7 +17,7 @@ import it.tdlight.jni.TdApi
 class SendReasonOnCallDecline(
     vararg reasons: String
 ) : TdInteractiveScenario() {
-    private val buttons = reasons.map { it.take(21) }
+    private val buttons = reasons.map { it.take(21) toState "/SendReasonText/$it" }.toTypedArray()
     private lateinit var telegramApi: TdTelegramApi
 
     override val helpMarkdownText =
@@ -39,22 +41,18 @@ class SendReasonOnCallDecline(
             val user = telegramApi.send(TdApi.GetUser(userId.toLong()))
 
             context.session["user_to_send_call_dismiss_reason"] = user
-            reactions.say("${"telephone_receiver".asEmojiUnicode} You've declined incoming call from ${user.firstName} ${user.lastName}.\n"
-                    + "Click one of the buttons below if you'd like to send a reason text back to the user.", buttons)
+
+            reactions.say("${"telephone_receiver".asEmojiUnicode} You've declined incoming call from *${user.firstName} ${user.lastName}*.\n"
+                    + "Click one of the buttons below to send a reason text back to them.", ParseMode.MARKDOWN)
+            reactions.buttons(*buttons)
         }
 
-        state("SendReasonText") {
-            activators {
-                buttons.forEach {
-                    regex(Regex.escape(it)).onlyIfInSession("user_to_send_call_dismiss_reason")
-                }
-            }
-
-            action {
-                reasons.find { it.startsWith(request.input) }?.also { reason ->
+        reasons.forEach { reason ->
+            state("/SendReasonText/$reason") {
+                action {
                     val user = context.session.remove("user_to_send_call_dismiss_reason") as TdApi.User
                     telegramApi.sendTextMessage(user.id, reason)
-                    reactions.say("Okay, I've sent a reason \"$reason\" to ${user.firstName} ${user.lastName}.")
+                    reactions.say("Okay, I've sent a reason _\"$reason\"_ to *${user.firstName} ${user.lastName}*.", ParseMode.MARKDOWN)
                 }
             }
         }
